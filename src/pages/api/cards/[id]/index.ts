07f1f3ef9a5ea255bc3fbf72c45da8f71ecb0e79
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-04-24 19:12:48 +0800

    Finish edit card

---
+++ src/pages/api/cards/[id]/index.ts
@@ -0,0 +1,63 @@
+import { NextApiHandler } from "next"
+import { getSession } from "next-auth/client"
+import mongooseConnect from "../../../../middlewares/mongooseConnect"
+import CardModel from "../../../../models/CardModel"
+import UserModel from "../../../../models/UserModel"
+
+interface EditCardRequestBody {
+  type: string
+  balance: number
+  name: string
+  issuer: string
+}
+
+interface EditCardResponse extends EditCardRequestBody {
+  cardId: string
+  userId: string
+}
+
+const editCard = async ({
+  cardId,
+  userId,
+  type,
+  balance,
+  name,
+  issuer,
+}: EditCardResponse): Promise<EditCardResponse> => {
+  const card = await CardModel.findById(cardId)
+  card.type = type
+  card.balance = balance
+  card.name = name
+  card.issuer = issuer
+  card.userId = userId
+  card.transactions = []
+  await card.save()
+
+  return {
+    cardId,
+    userId,
+    type,
+    balance,
+    name,
+    issuer,
+  }
+}
+
+const CardSpecHandler: NextApiHandler = async (req, res): Promise<void> => {
+  const session = await getSession({ req })
+  const user = await UserModel.findOne({ email: session.user.email }).lean()
+  const cardId = req.query.id as string
+
+  switch (req.method) {
+    case "PUT": {
+      const data = req.body as EditCardRequestBody
+      await editCard({ ...data, cardId, userId: user.id })
+      return res.json({ success: true })
+    }
+    default: {
+      throw Error("Method not supported.")
+    }
+  }
+}
+
+export default mongooseConnect(CardSpecHandler)
