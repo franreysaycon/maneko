55b654bf733ea963c71c40215184db9ac5f4a3d6
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-04-25 22:59:25 +0800

    Add meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import React from "react"
 import theme from "../theme"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { Provider } from "next-auth/client"
+import Head from "next/head"
 import "typeface-poppins"
 
 const queryClient = new QueryClient()
@@ -12,6 +13,30 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ChakraProvider theme={theme} resetCSS>
     <QueryClientProvider client={queryClient}>
       <Provider session={pageProps.session}>
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <link
+            href="favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link href="apple-touch-icon" rel="apple-touch-icon" />
+          <link href="/manifest.json" rel="manifest" />
+          <title>Maneko - Track Your PH Accounts</title>
+        </Head>
         <Component {...pageProps} />
       </Provider>
     </QueryClientProvider>
