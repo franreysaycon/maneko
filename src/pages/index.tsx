dc2ca455874a90a7a03ad55db427b37306e24306
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-04-25 19:29:01 +0800

    Add server side props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import AuthGuard from "../component/AuthGuard"
 import axios from "axios"
 import { CardT } from "../component/CardCollection/types"
 import { useQuery } from "react-query"
+import { getSession } from "next-auth/client"
 
 const getCards = async (): Promise<CardT[]> => {
   const response = await axios.get<CardT[]>("/api/cards")
@@ -21,6 +22,11 @@ const Homepage: React.FC = () => {
       </ManekoPage>
     </AuthGuard>
   )
+}
+
+export async function getServerSideProps(ctx) {
+  const session = await getSession(ctx)
+  return { props: { session } }
 }
 
 export default Homepage
